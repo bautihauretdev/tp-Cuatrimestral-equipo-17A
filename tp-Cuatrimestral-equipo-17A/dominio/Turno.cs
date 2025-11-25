e1027b3ff3a1e79a8ea1496413193d23805cdd50
natudellasavia <80315756+natudellasavia@users.noreply.github.com>
2025-11-24 23:26:23 -0300

    Implementación de listado Socios en la clase Turno para guardar los socios de cada Turno

--- tp-Cuatrimestral-equipo-17A/dominio/Turno.cs
+++ tp-Cuatrimestral-equipo-17A/dominio/Turno.cs
@@ -12,5 +12,21 @@ namespace dominio
         public DateTime Fecha { get; set; }
         public int CapacidadMaxima { get; set; }
         public int Ocupados { get; set; }   // Cantidad socios que asisten, acumulable
+        public List<Socio> Socios { get; set; } = new List<Socio>(); // Listado de socios en el turno
+
+
+        // Método para agregar socio respetando la capacidad máxima
+        public bool AgregarSocio(Socio socio)
+        {
+            if (Socios.Count < CapacidadMaxima)
+            {
+                Socios.Add(socio);
+                Ocupados = Socios.Count; // actualizar Ocupados
+                return true;
+            }
+            return false; // No se pudo agregar
+        }
+
+
     }
 }
